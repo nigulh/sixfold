b01716036d73c2d0a4571e4fcbe108bcafad5496
Hendrik Nigul <hendrik.nigul@gmail.com>
2022-01-19 19:57:18 +0200

    update test

--- __tests__/SphereDistanceCalculator.spec.js
+++ __tests__/SphereDistanceCalculator.spec.js
@@ -1,10 +1,21 @@
 SphereDistanceCalculator = require("../src/SphereDistanceCalculator");
 
+let calculator = new SphereDistanceCalculator();
+
 describe('Sphere distance', () => {
     it('distance to self is zero', () => {
-        expect(new SphereDistanceCalculator().findDistance(3, 2, 3, 2)).toBeCloseTo(0.0, 7);
+        expect(calculator.findDistance(3, 2, 3, 2)).toBeCloseTo(0.0, 7);
     });
-    it('distance to pole is half perimeter', () => {
-        expect(new SphereDistanceCalculator().findDistance(3, 2, -3, 180 + 2)).toBeCloseTo(Math.PI, 7);
+    describe("distance to antipode is half perimeter", () => {
+        Object.entries({
+            "origin": [2, 3],
+            "random": [4, 5],
+            "pole": [0, 90],
+            "equator": [0, 0]
+        }).forEach(([key, [N, E]]) => {
+            it(key, () => {
+                expect(calculator.findDistance(2, 3, N, E) + calculator.findDistance(N, E, -2, 183)).toBeCloseTo(Math.PI, 7);
+            });
+        });
     });
 });
