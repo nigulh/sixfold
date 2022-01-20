3a2374c5d8b9b139d061dd74f46bf0e2e16f6c9d
Hendrik Nigul <hendrik.nigul@gmail.com>
2022-01-20 21:14:43 +0200

    Add cors for swagger. Working ping endpoint via swagger

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(cors({origin: '*'}));
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+app.get('/ping/:message', (req, res) => {
+    let message = req.params.message;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({echo: message + message}));
 });
 
 app.listen(PORT, HOST);
