bc9327afcc9dfb9a46577afbbd3ed67f25a523ee
Hendrik Nigul <hendrik.nigul@gmail.com>
2022-01-25 20:34:30 +0200

    airports

--- src/Database.ts
+++ src/Database.ts
@@ -1,8 +1,6 @@
-import {Airport} from "./models/airport";
-
 export function getAllAirports() {
-    let airport = new Airport("A", "Aaaa", "B", "E", 20, 30);
-    return [airport];
+    return '415,"Lennart Meri Tallinn Airport","Tallinn-ulemiste International","Estonia","TLL","EETN",59.41329956049999,24.832799911499997,131,2,"E","Europe/Tallinn","airport","OurAirports"\n' +
+        '416,"Tartu Airport","Tartu","Estonia","TAY","EETU",58.3074989319,26.690399169900004,219,2,"E","Europe/Tallinn","airport","OurAirports"';
 }
 export function getAll() {
     return [];
--- src/database/AirportProvider.ts
+++ src/database/AirportProvider.ts
@@ -1,4 +1,5 @@
 import {getAllAirports} from '../Database'
+import {Airport} from "../models/airport";
 
 export class AirportProvider
 {
@@ -6,11 +7,17 @@ export class AirportProvider
     }
 
     findAll() {
-        return getAllAirports();
+        let allAirports = getAllAirports();
+        return allAirports.split("\n").map(this.convertCsvToAirport);
     }
 
     findById(id: string)
     {
-        return getAllAirports().find(x => x.iataCode == id);
+        return this.findAll().find(x => x.iataCode == id);
+    }
+
+    private convertCsvToAirport(row) {
+        let data = JSON.parse("[" + row + "]");
+        return new Airport(data[4], data[1], data[2], data[3], data[6], data[7]);
     }
 }
