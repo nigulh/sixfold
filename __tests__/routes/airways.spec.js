b37a60a92884f895c01275a888453370c9886fbb
Hendrik Nigul <hendrik.nigul@gmail.com>
2022-01-26 20:47:07 +0200

    successful "mocking"

--- __tests__/routes/airways.spec.js
+++ __tests__/routes/airways.spec.js
@@ -1,13 +1,13 @@
 const request = require('supertest')
 const app = require('../../dist/server')
 const {Airport} = require("../../dist/models/airport");
-//const Database = require("../../dist/Database");
+const Database = require("../../dist/Database");
 
-//Database.mockAllAirports("A,B,C,D,E,F,G,H");
+Database.setAllAirports('"A","B","C","D","E","F","G","H"');
 describe('Airways Endpoints', () => {
     it('should get all airports', async () => {
         const res = await request(app).get('/airports');
         expect(res.statusCode).toEqual(200);
-        expect(res.body).toEqual([ new Airport("A4", "X", "C")]);
+        expect(res.body).toEqual([ new Airport("E", "B", "C", "D", "G", "H")]);
     })
 })
