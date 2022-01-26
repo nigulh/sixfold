edbda51c408e55e71cc9377753a89c9dcd70d046
Hendrik Nigul <hendrik.nigul@gmail.com>
2022-01-26 20:38:35 +0200

    failing airports test

--- src/Database.ts
+++ src/Database.ts
@@ -1,8 +1,12 @@
 export function getAllAirports() {
-    return '415,"Lennart Meri Tallinn Airport","Tallinn-ulemiste International","Estonia","TLL","EETN",59.41329956049999,24.832799911499997,131,2,"E","Europe/Tallinn","airport","OurAirports"\n' +
+    return mocks.airports ?? '415,"Lennart Meri Tallinn Airport","Tallinn-ulemiste International","Estonia","TLL","EETN",59.41329956049999,24.832799911499997,131,2,"E","Europe/Tallinn","airport","OurAirports"\n' +
         '416,"Tartu Airport","Tartu","Estonia","TAY","EETU",58.3074989319,26.690399169900004,219,2,"E","Europe/Tallinn","airport","OurAirports"';
 }
 export function getAll() {
     return [];
 }
-
+let mocks = { airports: undefined };
+export function setAllAirports(value)
+{
+    mocks.airports = value;
+}
