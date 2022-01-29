81db8e0cb1f503366e53739813d8e78702f961f2
Hendrik Nigul <hendrik.nigul@gmail.com>
2022-01-29 08:22:32 +0200

    read airports from file

--- src/Database.ts
+++ src/Database.ts
@@ -1,5 +1,13 @@
+const fs = require('fs');
+
+function content(path) {
+    let ret = fs.readFileSync(path).toString();
+    return ret;
+}
+
 export function getAllAirports() {
-    return mocks.airports ?? '415,"Lennart Meri Tallinn Airport","Tallinn-ulemiste International","Estonia","TLL","EETN",59.41329956049999,24.832799911499997,131,2,"E","Europe/Tallinn","airport","OurAirports"\n' +
+    let c = content('data/airports.dat');
+    return mocks.airports ?? c ?? '415,"Lennart Meri Tallinn Airport","Tallinn-ulemiste International","Estonia","TLL","EETN",59.41329956049999,24.832799911499997,131,2,"E","Europe/Tallinn","airport","OurAirports"\n' +
         '416,"Tartu Airport","Tartu","Estonia","TAY","EETU",58.3074989319,26.690399169900004,219,2,"E","Europe/Tallinn","airport","OurAirports"';
 }
 export function getAll() {
