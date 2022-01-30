d469049e0a0d97e616ce00ecd548ba4565ab7256
Hendrik Nigul <hendrik.nigul@gmail.com>
2022-01-30 19:34:58 +0200

    refactor

--- src/shortestPath/Dijkstra.ts
+++ src/shortestPath/Dijkstra.ts
@@ -1,33 +1,36 @@
 import {Vertex} from "./Graph";
 import {ShortestPath, ShortestPathTask} from "./ShortestPathTask";
+
 const PriorityQueue = require('js-priority-queue');
 
 export class Dijkstra implements ShortestPath{
 
     findShortestPath(task: ShortestPathTask): [distance: number, path: Array<Vertex>] {
-        let graph = task.graph
-        let source = task.source;
-        let target = task.target;
         let queue = new PriorityQueue({comparator: function([a1, a2, a], [b1, b2, b]) {
             return (a || 0) - (b || 0);
         }});
         let visited: { [K in Vertex]?: number } = {}
         let prevNode: { [K in Vertex]?: Vertex } = {}
 
-        queue.queue([source, undefined, 0]);
-        visited[source] = 0;
+        queue.queue([task.source, undefined, 0]);
+        let insertedCounter = 0, processedCounter = 0;
         while(queue.length > 0)
         {
             let [cur, prev, curDistance] = queue.dequeue();
             if ((visited[cur] ?? Infinity) < curDistance) continue;
             console.log("expanding from node: " + [cur, curDistance]);
+            processedCounter += 1;
             visited[cur] = curDistance;
             prevNode[cur] = prev;
-            if (cur == target) return [curDistance, this.backtrackPath(target, prevNode)];
+            if (cur == task.target) {
+                console.log({inserted: insertedCounter, processed: processedCounter});
+                return [curDistance, this.backtrackPath(cur, prevNode)];
+            }
 
-            for (let next of graph.getAdjacentFrom(cur))
+            for (let next of task.graph.getAdjacentFrom(cur))
             {
                 queue.queue([next, cur, curDistance + 1]);
+                insertedCounter += 1;
             }
         }
         return [Infinity, []];
